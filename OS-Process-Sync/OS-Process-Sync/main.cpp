0c187fa3a0f641899afc0dee3860b0d67a3118a2
XuYanci <grandy.wind@gmail.com>
2019-05-09 23:31:11 +0800

    Init Repo for OS

---
+++ OS-Process-Sync/OS-Process-Sync/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  OS-Process-Sync
+//
+//  Created by Yanci on 2019/5/9.
+//  Copyright © 2019 Yanci. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    /// 生产者消费者问题
+    /// 单缓存区问题
+    /// 多缓存区问题
+    /// 读写问题
+    
+    /// 哲学家问题
+    
+    return 0;
+}
