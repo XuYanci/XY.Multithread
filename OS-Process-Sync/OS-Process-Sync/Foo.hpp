d23e4be058637807dde33f62d9f17ba70d0a1a28
yanci <grandy.wind@gmail.com>
2019-11-04 23:22:05 +0800

    1. add solution

--- OS-Process-Sync/OS-Process-Sync/Foo.hpp
+++ OS-Process-Sync/OS-Process-Sync/Foo.hpp
@@ -9,32 +9,51 @@
 #ifndef Foo_hpp
 #define Foo_hpp
 #include <functional>
+
 using namespace std;
 using namespace std::placeholders;
 
 #include <stdio.h>
+#include <semaphore.h>
+#include <pthread.h>
+
 class Foo {
 public:
+    
+    sem_t _sem_first;
+    sem_t _sem_second;
+    sem_t _sem_third;
+    
+    
     Foo() {
-        
+        sem_init(&_sem_first,0,0);
+        sem_init(&_sem_second,0,0);
+        sem_init(&_sem_third,0,0);
     }
+    
     
     void first(function<void()> printFirst) {
         
         // printFirst() outputs "first". Do not change or remove this line.
         printFirst();
+        
+        sem_post(&_sem_first);
     }
     
     void second(function<void()> printSecond) {
-        
+        sem_wait(&_sem_first);
         // printSecond() outputs "second". Do not change or remove this line.
         printSecond();
+        sem_post(&_sem_second);
     }
     
     void third(function<void()> printThird) {
-        
+        sem_wait(&_sem_second);
         // printThird() outputs "third". Do not change or remove this line.
         printThird();
+        sem_post(&_sem_third);
+        
     }
+    
 };
 #endif /* Foo_hpp */
