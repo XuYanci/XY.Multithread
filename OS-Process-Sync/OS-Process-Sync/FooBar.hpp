426592088915ddb781816c3a72b726dc6bd901e0
yanci <grandy.wind@gmail.com>
2019-11-05 08:26:34 +0800

    commit some

--- OS-Process-Sync/OS-Process-Sync/FooBar.hpp
+++ OS-Process-Sync/OS-Process-Sync/FooBar.hpp
@@ -12,30 +12,40 @@
 using namespace std;
 using namespace std::placeholders;
 #include <stdio.h>
+
+#include <stdio.h>
+#include <semaphore.h>
+#include <pthread.h>
+
 class FooBar {
 private:
     int n;
-    
+    sem_t _sem_first;
+    sem_t _sem_second;
 public:
     FooBar(int n) {
         this->n = n;
+        sem_init(&_sem_first,0,1);
+        sem_init(&_sem_second,0,0);
     }
     
     void foo(function<void()> printFoo) {
         
         for (int i = 0; i < n; i++) {
-            
+            sem_wait(&_sem_first);
             // printFoo() outputs "foo". Do not change or remove this line.
             printFoo();
+            sem_post(&_sem_second);
         }
     }
     
     void bar(function<void()> printBar) {
         
         for (int i = 0; i < n; i++) {
-            
+            sem_wait(&_sem_second);
             // printBar() outputs "bar". Do not change or remove this line.
             printBar();
+            sem_post(&_sem_first);
         }
     }
 };
