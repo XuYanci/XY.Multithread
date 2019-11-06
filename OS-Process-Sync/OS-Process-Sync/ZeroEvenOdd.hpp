c4b92ea4b098b14963dc476d209771122cc8324c
yanci <grandy.wind@gmail.com>
2019-11-07 00:25:56 +0800

    solution 1116. 打印零与奇偶数

--- OS-Process-Sync/OS-Process-Sync/ZeroEvenOdd.hpp
+++ OS-Process-Sync/OS-Process-Sync/ZeroEvenOdd.hpp
@@ -9,31 +9,60 @@
 #ifndef ZeroEvenOdd_hpp
 #define ZeroEvenOdd_hpp
 #include <functional>
+#include <semaphore.h>
 
 using namespace std;
 using namespace std::placeholders;
 
 #include <stdio.h>
+#include <semaphore.h>
+#include <semaphore.h>
 class ZeroEvenOdd {
 private:
     int n;
-    
+    int i = 1;
+    sem_t _sem_zero;
+    sem_t _sem_odd;
+    sem_t _sem_even;
 public:
     ZeroEvenOdd(int n) {
         this->n = n;
+        sem_init(&_sem_zero,0,1);
+        sem_init(&_sem_odd,0,0);
+        sem_init(&_sem_even,0,0);
     }
     
     // printNumber(x) outputs "x", where x is an integer.
     void zero(function<void(int)> printNumber) {
-        
-    }
-    
-    void even(function<void(int)> printNumber) {
-        
+        for (int ii = 0; ii < n; ii++) {
+            sem_wait(&_sem_zero);
+            
+            printNumber(0);
+            if (i % 2 == 1) {
+                sem_post(&_sem_odd);
+            }
+            else {
+                sem_post(&_sem_even);
+            }
+        }
     }
     
     void odd(function<void(int)> printNumber) {
-        
+        for (int ii = 0; ii < (n % 2 + n / 2); ii++) {
+            sem_wait(&_sem_odd);
+            
+            printNumber(i++);
+            sem_post(&_sem_zero);
+        }
+    }
+    
+    void even(function<void(int)> printNumber) {
+        for (int ii = 0; ii < n / 2; ii++) {
+            sem_wait(&_sem_even);
+           
+            printNumber(i++);
+            sem_post(&_sem_zero);
+        }
     }
 };
 #endif /* ZeroEvenOdd_hpp */
